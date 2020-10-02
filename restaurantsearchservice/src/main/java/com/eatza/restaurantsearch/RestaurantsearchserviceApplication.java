f10b7b2283b77be0397d95a3cedf0ad11bf606d2
palani16 <palani993@gmail.com>
2020-10-02 14:17:57 +0530

    updated properties for eureka

--- restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
+++ restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.eatza.restaurantsearch.config.JwtFilter;
