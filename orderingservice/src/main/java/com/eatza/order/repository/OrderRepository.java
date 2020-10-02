a092a05e29a7689ab2c7364193c5c700167c2bd5
palani16 <palani993@gmail.com>
2020-10-02 11:26:38 +0530

    updated changes

--- orderingservice/src/main/java/com/eatza/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eatza.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eatza.order.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
